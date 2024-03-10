8a9833b94d0fbb043a4af7606967bd874923c1b2
SkyEye_FAST <skyeyefast@teahou.se>
2024-03-10 21:23:27 +0800

    Update

--- pack.py
+++ pack.py
@@ -2,6 +2,7 @@
 """Minecraft本地化键名资源包生成器"""
 
 import json
+import re
 import zipfile as z
 from pathlib import Path
 
@@ -16,8 +17,24 @@ with open(P / "source" / "en_us.json", "rb") as s:
 with open(P / "key.json", "w", encoding="utf-8") as f:
     json.dump({k: k for k in source.keys()}, f, indent=2)
 
+for key, value in source.items():
+    if key.startswith("translation."):
+        continue
+    if "%" in value:
+        arguments = re.findall(r"%\d*\$?[sd]", value)
+        arguments_list = [f"({arg})" for arg in arguments]
+        source[key] = re.sub(
+            r"%\d+\$s", r"%s", f"{key} {" ".join(arguments_list)}"
+        ).rstrip()
+    else:
+        source[key] = key
+
+with open(P / "key_arg.json", "w", encoding="utf-8") as file:
+    json.dump(source, file, indent=2)
+
 # 生成资源包
 pack_dir = P / "key_language_pack.zip"
 with z.ZipFile(pack_dir, "w", compression=z.ZIP_DEFLATED, compresslevel=9) as f:
     f.write(P / "pack.mcmeta", arcname="pack.mcmeta")
     f.write(P / "key.json", arcname="assets/minecraft/lang/key.json")
+    f.write(P / "key_arg.json", arcname="assets/minecraft/lang/key_arg.json")
